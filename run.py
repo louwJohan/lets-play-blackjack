ddc3d4d1c86d70252117377a2f0648895b9ec2c7
Louw Johan Coetzee <Louwjohanc@gmail.com>
2022-01-30 18:05:36 +0000

    Initial commit add function to create a deck of cards

--- run.py
+++ run.py
@@ -1,3 +1,25 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def deck_of_cards():
+    """
+    The function generates a deck of cards from 2 lists and 
+    shuffles the deck
+    """
+    numbers = ["A", "2","3","4","5","6","7",
+    "8","9","10","J","Q","K"]
+    suite = ["S","H","C","D"]
+
+    deck = []
+    for number in numbers:
+        for shape in suite:
+            deck.append(number + shape)
+
+    random.shuffle(deck)
+    return deck
+
+
+def main():
+    game_deck = deck_of_cards()
+
+
+main()
